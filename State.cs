26d9bddcffd99d4da5d321119d2efee6d179490a
mstrykul1336 <112668967+mstrykul1336@users.noreply.github.com>
2022-09-09 00:56:23 -0700

    Add files via upload

---
+++ State.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.State
+{
+    public interface IBikeState
+    {
+        void Handle(BikeController controller);
+    }
+    public class BikeStateContext
+    {
+        public IBikeState CurrentState
+        {
+            get; set;
+        }
+        private readonly BikeController _bikeController;
+
+        public BikeStateContext(BikeController bikeController)
+        {
+            _bikeController = bikeController;
+        }
+
+        public void Transition()
+        {
+            CurrentState.Handle(_bikeController);
+        }
+        public void Transition(IBikeState state)
+        {
+            CurrentState = state;
+            CurrentState.Handle(_bikeController);
+        }
+    }
+}
